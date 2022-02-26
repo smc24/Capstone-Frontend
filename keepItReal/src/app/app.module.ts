6d19fb8c60f9dad7c2761da7c40f8354e70d5832
unknown <sasham_c@yahoo.com>
2022-02-26 12:54:00 -0600

    header component is done for now. I'll figure out how to add the shizuru font later

---
+++ keepItReal/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { CompanyTitleComponent } from './header/company-title/company-title.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    CompanyTitleComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
