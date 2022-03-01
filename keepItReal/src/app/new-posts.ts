25ee2fd491298577abcf103d3680a92fc163f641
unknown <sasham_c@yahoo.com>
2022-03-01 11:51:19 -0600

    created a separate ts file for a new post.

---
+++ keepItReal/src/app/new-posts.ts
@@ -0,0 +1,9 @@
+export class NewPosts{
+    name: any;
+    mentalConcern: any;
+
+    constructor(name: any, mentalConcern: any){
+        this.name = name;
+        this.mentalConcern = mentalConcern;
+    }
+}
