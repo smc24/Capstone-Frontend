244e12d4da1009c143ebe0c98cd3bfcdd8d41a42
unknown <sasham_c@yahoo.com>
2022-03-01 17:49:21 -0600

    can now post data from my backend database

--- keepItReal/src/app/posts/posts.component.ts
+++ keepItReal/src/app/posts/posts.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NewPosts } from '../new-posts';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-posts',
@@ -12,15 +13,26 @@ export class PostsComponent implements OnInit {
 
   postName: any;
   message: any;
+  posts: any;
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  onSubmit(name: any, mentalConcern: any){
     this.http.post<any>('http://localhost:8080/anonuser/posts/', {name: 'Samo', mentalConcern: 'something expressed'})
     .subscribe((post) => {
       this.postName = post.name;
       this.message = post.mentalConcern;
     })
+  }
+
+  
+
+  ngOnInit() {
+   this.http.get('http://localhost:8080/anonuser/posts/')
+   .subscribe(response=> {
+     this.posts=response;
+     console.log(response);
+   })
   }
 
 }
